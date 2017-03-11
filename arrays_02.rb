cf439eb4b5625d7194ff2f2854995f707c9596c8
Matt Redmon <mredmon333@gmail.com>
2017-03-10 22:19:52 -0500

    adding work on ruby docs arrays

--- arrays_02.rb
+++ arrays_02.rb
@@ -1,4 +1,4 @@
-
+=begin
 # ***PUBLIC CLASS METHODS***
 
 
@@ -145,3 +145,159 @@ a.delete_at(99) # => nil
 # array is changed instantly every time block called, not when iteration is over
 scores = [97, 42, 75]
 scores.delete_if { |score| score < 80 }  # => [97]
+
+# dig method- extracts nested value in an array
+a = [[1,[2,3]]]  # an array within an array within another array
+a.dig(0,1,1) # => 3
+             # element at position 0 in outer array [........]
+             # element at position 1 in middle array [1,[2,3]]
+             # element at position 1 in inner array [2,3]  -- which is a three, our answer
+a.dig(1,2,3) # => nil
+[42, {foo: :bar}].dig(1, :foo)  # => :bar
+
+# drop method - drops first n elements, returns rest of elements in an array
+a = [1,2,3,4,5,0]
+a.drop(3)  # => [4,5,0]
+
+# drop_while method - drops elements up to but not including 1st element for which block returns nil/false
+a = [1,2,3,4,5,0]
+a.drop_while { |i| i < 3 }  # => [3,4,5,0]
+
+# each method - calls given block for each element in self
+a = ['a','b','c']
+a.each { |x| print x, "--" }  # prints: "a -- b -- c --"
+
+# each_index method - same as each but passes index of element instead of element itself
+a = ['a','b','c']
+a.each_index { |x| print x, "--" }  # prints:  "0 -- 1 -- 2 --"
+
+# empty? method
+[].empty?  # => false
+
+# eql? method - returns true if self and other are same obj, or are both arrays with same concatenation
+
+# fetch method
+a = [11,22,33,44]
+a.fetch(1)         # => 22
+a.fetch(-1)        # => 44
+a.fetch(4, "cat")  # => "cat"
+# allows us to supply second argument (cat in our example) that is returned if the 1st arg
+# referenced lies outside of the array bounds, allowing us to avoid throwing an error
+
+# fill method
+a = ['a','b','c','d']
+a.fill('x')      # => ['x','x','x','x']
+a.fill('z',2,2)  # => ['x','x','z','z']  start at position 2 and fill 2
+a.fill('y',0..1) # => ['y','y','z','z']  fill positions 0 through 1
+a.fill {|i| i*i} # => [1,2,4,9]  operating on index since using i
+
+=end
+# find_index method
+# appears only use index not find_index
+a = ['a','b','c','d']
+a.index('b')  # => 1
+a.index('z')  # => nil
+a.index { |x| x == 'b' }  # => 1
+
+# first method
+a = ['q','r','s','t']
+a.first    # => 'q'
+a.first(2) # => ['q','r']
+
+# flatten method - NON DESTRUCTIVE VERSION
+s = [1,2,3]
+t = [4,5,6,[7,8]]
+a = [s,t,9,10]  #  => [[1,2,3],[4,5,6,[7,8]],9,10]
+a.flatten       #  => [1,2,3,4,5,6,7,8,9,10]
+
+a = [1,2,[3,[4,5]]]
+a.flatten(1)  # =>  [1,2,3,[4,5]]  with optional level arg of 1 only one level off multi level array is flattened
+
+# flatten! method  - DESTRUCTIVE VERSION
+a = [1,2,[3,[4,5]]]
+a.flatten!   # => [1,2,3,4,5]
+a.flatten!   # => nil   -- returns nil because line before already permanently flattened array
+a            # => [1,2,3,4,5]   -- flatten!  is DESTRUCTIVE
+
+# frozen? method
+# return true if array is frozen
+
+# hash method - compute a hash for for this array
+# two arrays with the same content will have the same hash code and will compare using eql?
+
+# include? method - returns true if given obj is present in self
+a = ['a','b','c','d']
+a.include('b')  # => true
+a.include('z')  # => false
+
+# index method - returns index of first obj in array
+a = ['a','b','c','d']
+a.index('b')            # => 1
+a.index('z')            # => nil
+a.index {|x| x == 'b'}  # => 1
+
+# initialize_copy   --- see replace method
+
+# insert method - inserts given values before the element with the given index
+a = ['a','b','c','d']
+a.insert(2,99)       # => ['a','b',99,'c','d']  inserts 99 at postion 2
+a.insert(-2,1,2,3)   # => ['a','b',99,'c',1,2,3,'d']   inserts # 1,2,3 at position -2
+
+# inspect  --> to_s method
+# to_s method is listed as alias for inspect method
+['a','b','c'].to_s   # => "[\"a\", \"b\", \"c\"]"   back slashes escaping the double quotes
+
+# join method
+['a','b','c','d'].join  # => "abcd"
+['a','b','c','d'].join("-")  # => "a-b-c-d"
+
+# keep_if method
+a = ['a','b','c','d','e','f']
+a.keep_if { |v| v =~ /[aeiou]/ }  # => ['a','e']
+
+# last method
+a = ['w','x','y','z']
+a.last     # => "z"
+a.last(2)  # => ['y','z']
+
+# length method
+[1,2,3,4,5].length  # => 5
+[].length           # => 0
+
+# map method - non Destructive version
+# map and collect can be used interchangably
+a = ['a','b','c','d']
+a.collect { |x| x + "!" }  # => ['a!', 'b!', 'c!', 'd!']
+a.map.with_index { |x,i| x * i }  # => ['', 'b', 'cc', 'ddd']  -- multiplying each element by its index
+
+# map! and collect! methods - DESTRUCTIVE version of above
+
+# max method - returns obj in array with the maximum value
+a = ["albatross", "dog", "horse"]
+a.max     # => "horse"
+a.max { |a,b| a.length <=> b.length } # => "albatross"  -- code gives longest word
+a.max(2)  # => ["horse", "dog"]
+a.max(2) { |a,b| a.length <=> b.length } # => ["albatross", "horse"] -- longest word, 2nd longest word
+
+
+# min method - returns obj with the minimum value
+a = ["albatross", "dog", "horse"]
+a.min  # => "albatross"
+a.min { |a,b| a.length <=> b.length }  # => "dog" --  shortest word
+a.min(2) # => ["albatross", "dog"]
+a.max(2) { |a,b| a.length <=> b.length }  # => ["dog", "horse"] -- shortest and 2nd shortest word
+
+# pack method -- see documentation- extensive explanation
+
+# permutation method
+a = [1,2,3]
+a.permutation.to_a     # => [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]+
+a.permutation(1).to_a  # => [[1],[2],[3]]
+a.permutation(2).to_a  # => [[1,2],[1,3],[2,1],[2,3],[3,1],[3,2]]
+a.permutation(3).to_a  # => [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+# pop method - removes last element from itself and returns it, nil if empty array
+a = ['a','b','c','d']
+a.pop    # => 'd'    # this is a DESTRUCTIVE method
+a.pop(2) # => ['b', 'c']
+a        # => ['a']
